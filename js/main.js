b3ee928896e68730e9b587cab677a6f2e9034a39
GimenaRiveros <gimena1001riveros@gmail.com>
2022-12-11 18:18:39 -0300

    Controlando el contador de movimientos y hits. MGR

--- js/main.js
+++ js/main.js
@@ -2,15 +2,21 @@
 let uncoverCards = 0;
 let cardFirst, cardSecond = null;
 let resultFirst, resultSecond = null;
-
+let moves = 0;
+let hits = 0;
+// LINK TO HTML
+let showMoves = document.getElementById('moved');
+let showHits = document.getElementById('hits');
 
 // GENERAMOS NUMEROS ALEATORIOS
 let randomNumbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 randomNumbers = randomNumbers.sort(() => {return Math.random() - 0.5});
+console.log(randomNumbers);
 
 // MAIN FUNCTION
 function uncover(id) {
     uncoverCards++;
+    console.log(uncoverCards);
     
     // ES LA TARJETA UNO O DOS?
     if(uncoverCards == 1) {
@@ -19,6 +25,26 @@ function uncover(id) {
         cardFirst.innerHTML = resultFirst;
         //FIRST BUTTON DISABLE
         cardFirst.disabled = true;
+    } else if(uncoverCards == 2) {
+        // SHOW THE SECOND CARD
+        cardSecond = document.getElementById(id);
+        resultSecond = randomNumbers[id];
+        cardSecond.innerHTML = resultSecond;
+
+        //SECOND BUTTON DISABLE 
+        cardSecond.disabled = true;
+        
+        // INCREMENT MOVES
+        moves++;
+        showMoves.innerHTML = `Moves: ${moves}`;
+
+        if(resultFirst == resultSecond) {
+            // COUNTER UNCOVERCARDS START
+            uncoverCards = 0;
+            //HITS ++
+            hits++;
+            showHits.innerHTML = `Hits: ${hits}`;
+        }
     }
     
 }
